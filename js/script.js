4021b4c5326ae65b641cf890dadcec7b63bfc8d4
Yuriy Ankipovich <60668124+YuriyAnkipovich@users.noreply.github.com>
2021-04-01 13:40:21 +0300

    Добавил ещё иконки Awesome Font

--- js/script.js
+++ js/script.js
@@ -24,11 +24,11 @@ var main = {
     	var form = $('<div class="item input"><form name="form"><input class="out" type="text" name="out" readonly></form></div>');
     	var buttons = $(
             '<div class="item clean" onclick="calculator.clean(' + id + ')" data-toggle="tooltip" title="Очистить всё" data-placement="bottom">C</div>' +  
-            '<div class="item delete" onclick="calculator.delete_last()" data-toggle="tooltip" title="Удалить последний символ" data-placement="bottom">&larr;</div>' +  
-            '<div class="item" onclick="calculator.insert(' + id + ', \'+\')">+</div>' +  
-            '<div class="item" onclick="calculator.insert(' + id + ', \'-\')">-</div>' +  
-            '<div class="item" onclick="calculator.insert(' + id + ', \'*\')">&times;</div>' +  
-            '<div class="item" onclick="calculator.insert(' + id + ', \'/\')">&divide;</div>' +  
+            '<div class="item delete" onclick="calculator.delete_last(' + id + ')" data-toggle="tooltip" title="Удалить последний символ" data-placement="bottom"><i class="fas fa-backspace"></i></div>' +  
+            '<div class="item" onclick="calculator.insert(' + id + ', \'+\')"><i class="fas fa-plus"></i></div>' +  
+            '<div class="item" onclick="calculator.insert(' + id + ', \'-\')"><i class="fas fa-minus"></i></div>' +  
+            '<div class="item" onclick="calculator.insert(' + id + ', \'*\')"><i class="fas fa-times"></i></div>' +  
+            '<div class="item" onclick="calculator.insert(' + id + ', \'/\')"><i class="fas fa-divide"></i></div>' +  
             '<div class="item" onclick="calculator.insert(' + id + ', \'7\')">7</div>' + 
             '<div class="item" onclick="calculator.insert(' + id + ', \'8\')">8</div>' + 
             '<div class="item" onclick="calculator.insert(' + id + ', \'9\')">9</div>' + 
@@ -40,7 +40,7 @@ var main = {
             '<div class="item" onclick="calculator.insert(' + id + ', \'1\')">1</div>' + 
             '<div class="item" onclick="calculator.insert(' + id + ', \'2\')">2</div>' + 
             '<div class="item" onclick="calculator.insert(' + id + ', \'3\')">3</div>' +
-            '<div class="item equal" onclick="calculator.equal(' + id + ')" data-toggle="tooltip" title="Перед нажатием проверьте корректность введёного вами выражения" data-placement="bottom">=</div>' + 
+            '<div class="item equal" onclick="calculator.equal(' + id + ')" data-toggle="tooltip" title="Перед нажатием проверьте корректность введёного вами выражения" data-placement="bottom"><i class="fas fa-equals"></i></div>' + 
             '<div class="item zero" onclick="calculator.insert(' + id + ', \'0\')">0</div>' +
             '<div class="item" onclick="calculator.insert(' + id + ', \'.\')">.</div>');
         window.append(close, main_div.append(form, buttons));
